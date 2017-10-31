4480b896c0982407d3fd2b56558912e285a6854b
Jonas Wallin <jonas.wallin81@gmail.com>
2017-10-31 19:11:16 +0100

    added lab

---
+++ lab1/skeleton.R
@@ -0,0 +1,50 @@
+rm(list=ls())   # clear all old garbage
+graphics.off()  # reomve old plots
+##
+##
+# Basic R
+###
+###
+
+##
+# Exercise 1
+##
+
+
+
+
+##
+# Exercise 2
+##
+
+
+##
+##
+# sampling statistics
+###
+###
+
+##
+# Exercise 3
+##
+
+
+##
+# Exercise 4
+##
+
+##
+##
+# Bayesian statistics
+###
+###
+
+##
+# Exercise marble
+##
+
+##
+# Exercise Placenta previa
+##
+
+
